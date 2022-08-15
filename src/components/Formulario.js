e651fa3b8c19a1836fd44f9473d78be4fdd66a5f
nicolas andrade <nicolasandrade328@gmail.com>
2022-08-14 23:47:10 -0300

    instalamos formik para validar formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -9,13 +9,13 @@ const Formulario = () => {
           <div className="col-12 col-md-6">
             <Form.Group className="mb-3 px-3" controlId="formNombre">
               <Form.Label className="text-light">Nombre:</Form.Label>
-              <Form.Control type="text" placeholder="ingrese nombre" />
+              <Form.Control type="text" placeholder="ingrese nombre" minLength={4} maxLength={14} required/>
             </Form.Group>
           </div>
           <div className="col-12 col-md-6">
-            <Form.Group className="mb-3 px-3" controlId="formApellido">
+            <Form.Group className="mb-3 px-3" controlId="formApellido" >
               <Form.Label className="text-light">Apellido:</Form.Label>
-              <Form.Control type="text" placeholder="ingrese Apellido" />
+              <Form.Control type="text" placeholder="ingrese Apellido" minLength={4} maxLength={14} required />
             </Form.Group>
           </div>
         </div>
@@ -24,13 +24,13 @@ const Formulario = () => {
           <div className="col-12 col-md-6">
             <Form.Group className="mb-3 px-3" controlId="formDNI">
               <Form.Label className="text-light">DNI:</Form.Label>
-              <Form.Control type="number" placeholder="ingrese DNI" />
+              <Form.Control type="number" placeholder="ingrese DNI" minLength={7} maxLength={9} required/>
             </Form.Group>
           </div>
           <div className="col-12 col-md-6">
             <Form.Group className="mb-3 px-3" controlId="formEmail">
               <Form.Label className="text-light">Email:</Form.Label>
-              <Form.Control type="email" placeholder="ingrese email" />
+              <Form.Control type="email" placeholder="ingrese email" minLength={12} maxLength={40} required />
             </Form.Group>
           </div>
         </div>
